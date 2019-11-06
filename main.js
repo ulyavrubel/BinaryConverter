b7f37dd4e9fef92935cf1bcd7ffb10373fb47ed4
ulyavrubel <uvrubel@gmail.com>
2019-11-06 11:37:05 +0800

    --

---
+++ main.js
@@ -0,0 +1,13 @@
+function binaryAgent(str) {
+  let newArr = str.split(" ");
+  let result = [];
+  for (let i = 0; i < newArr.length; i++) {
+    result.push(String.fromCharCode(parseInt(newArr[i], 2)));
+  }
+  var resultStr = result.join("");
+  document.getElementById('outputText').innerHTML = resultStr;
+}
+
+
+
+// console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
